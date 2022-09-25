8ffdf0b80dd435c601bcd0ad276d0099e1cf60a8
Sererim <danielleymanw@gmail.com>
2022-09-25 23:05:44 +0300

    Function that works with finding the distance between two points in 3D space returns an Error about Fucntion and double, problems is probably with variables declaration within the class or outside of it.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,71 @@
-﻿int Main()
+﻿
+
+int Main()
 {
-    Distance AandB = new Distance();
-    
-    
+    double[] A = new double[3] {0,0,0};
+    double[] B = new double[3] {0,0,0};
+
+
+
+    string num = "NULL";
+
+    string key = "-1";
+    while(true)
+    {
+        Console.WriteLine("Enter 1 to find out if number of five digits is a palindrome or not.\n" +
+        "Enter 2 to print a list of all cubes from 1 to N.\n" +
+        "Enter 3 to find distance between two points in 3D space.\n" +
+        "Enter 0 to terminate the program."
+        );
+
+        key = Console.ReadLine();
+
+        string pal = "NULL";
+
+        int n = 0;
+
+
+        switch(key)
+        {
+            case "1":
+                Console.WriteLine("Enter a number to find if it is a palindrome or not.");
+                pal = Console.ReadLine();
+                Homework.Palindrome(pal);
+                break;
+            case "2":
+                Console.WriteLine("Enter a number to print a list of cubes.");
+                pal = Console.ReadLine();
+                n = System.Int32.Parse(pal);
+                Homework.Cube(n);
+                break;
+            case "3":
+                Console.WriteLine("Enter the coordinates of the first point in 3D space.");
+                for(int i = 0; i < 3; i++)
+                {
+                    num = Console.ReadLine();            
+                    A[i] = System.Double.Parse(num);
+                }
+                Console.WriteLine("Enter the coordinates of the second point in 3D space.");
+                for(int i = 0; i < 3; i++)
+                {
+                    num = Console.ReadLine();            
+                    B[i] = System.Double.Parse(num);
+                }
+
+                Distance r = new Distance(A,B);
+                Console.WriteLine($"The distance between two points in 3D space is {r.GetDistance}");
+                break;
+
+            case "0":
+                Console.WriteLine("Program is terminated.");
+                return 0;
+
+            default:
+                break;
+        }
+
+
+    }    
     return 0;
 }
 Main();
@@ -46,34 +109,21 @@ class Distance
         A = vecA;
         B = vecB;
     }
-    public void SetDistance()
-    {
-        string num = "NULL";
-        Console.WriteLine("Enter the coordinates of the first point in 3D space.");
-        for(int i = 0; i < 3; i++)
-        {
-            num = Console.ReadLine();            
-            A[i] = System.Double.Parse(num);
-        }
-            
-        Console.WriteLine("Enter the coordinates of the second point in 3D space.");
-        num = Console.ReadLine();
-        for(int i = 0; i < 3; i++)
-        {
-            num = Console.ReadLine();            
-            B[i] = System.Double.Parse(num);
-        }
-    }
 
     public double GetDistance()
     {
-        double d = Math.Sqrt(Math.Pow((B[0] - A[0]),2) + Math.Pow((B[1] - A[1]),2) + Math.Pow((B[2] - A[2]),2));
+        double d = 0;
+        d = Math.Sqrt(Math.Pow((B[0] - A[0]),2) + Math.Pow((B[1] - A[1]),2) + Math.Pow((B[2] - A[2]),2));
         return d;
     }
 
 }
 
+/*
+
 class LookForPalindrome
 {
-    
+
 }
+
+*/
